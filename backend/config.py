7bcbfb00e2ad6c0162d8ed9d91887e8096a2eded
Cynthea <52155036+cyantpea@users.noreply.github.com>
2025-04-22 21:27:02 -0600

    Fix config

--- backend/config.py
+++ backend/config.py
@@ -1,44 +1,19 @@
-"""Application configuration module.
-
-Args:
-    settings (Settings): The settings for the application
-"""
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
-    """Model for application settings.
+    """Model for application settings."""
 
-    Any default settings will be overwritten by environment variables.
-    """
-
-    app_title: str
-    app_description: str
-    db_url: str
-    db_sqlite: bool
-    jwt_algorithm: str
-    jwt_cookie_key: str
-    jwt_duration: int
-    jwt_issuer: str
+    app_title: str = "pony express"
+    app_description: str = "A social messaging app"
+    db_url: str = "sqlite:///backend/database/development.db"
+    db_sqlite: bool = True
+    jwt_algorithm: str = "HS256"
+    jwt_cookie_key: str = "pony-express-token"
+    jwt_duration: int = 3600
+    jwt_issuer: str = "http://127.0.0.1"
     jwt_secret_key: str = "super-secret-key"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
-def get_settings() -> Settings:
-    """Get the application settings."""
-
-    return Settings(
-        app_title="pony express",
-        app_description="A social messaging app",
-        db_url="sqlite:///backend/database/development.db",
-        db_sqlite=True,
-        jwt_algorithm="HS256",
-        jwt_cookie_key="pony-express-token",
-        jwt_duration=3600,
-        jwt_issuer="http://127.0.0.1",
-    )
-
-
-settings = get_settings()
+settings = Settings()
